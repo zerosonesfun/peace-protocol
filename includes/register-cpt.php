36cff8a83af385cc01caf265a40fec48b3b8c3b0
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2025-06-23 22:15:32 -0400

    Create register-cpt.php

---
+++ includes/register-cpt.php
@@ -0,0 +1,33 @@
+<?php
+defined('ABSPATH') || exit;
+
+add_action('init', function () {
+    $labels = [
+        'name' => __('Peace Logs', 'peace-protocol'),
+        'singular_name' => __('Peace Log', 'peace-protocol'),
+        'menu_name' => __('Peace Logs', 'peace-protocol'),
+        'name_admin_bar' => __('Peace Log', 'peace-protocol'),
+        'add_new' => __('Add New', 'peace-protocol'),
+        'add_new_item' => __('Add New Peace Log', 'peace-protocol'),
+        'new_item' => __('New Peace Log', 'peace-protocol'),
+        'edit_item' => __('Edit Peace Log', 'peace-protocol'),
+        'view_item' => __('View Peace Log', 'peace-protocol'),
+        'all_items' => __('All Peace Logs', 'peace-protocol'),
+        'search_items' => __('Search Peace Logs', 'peace-protocol'),
+        'not_found' => __('No peace logs found.', 'peace-protocol'),
+        'not_found_in_trash' => __('No peace logs found in Trash.', 'peace-protocol'),
+    ];
+
+    register_post_type('peace_log', [
+        'labels' => $labels,
+        'public' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_icon' => 'dashicons-heart',
+        'supports' => ['title', 'editor', 'custom-fields'],
+        'capability_type' => 'post',
+        'map_meta_cap' => true,
+        'exclude_from_search' => true,
+        'has_archive' => false,
+    ]);
+});
